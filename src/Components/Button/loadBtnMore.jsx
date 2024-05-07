2c3ba35a6b9daf73e1b3d2c462fb2c12d2639e53
exigibil <adrian.topa04@gmail.com>
2024-05-07 22:49:36 +0300

    Update loadBtnMore.jsx

--- src/Components/Button/loadBtnMore.jsx
+++ src/Components/Button/loadBtnMore.jsx
@@ -1,4 +1,4 @@
-import styles from "../Button/Button.module.css";
+import styles from "../Button/LoadBtnMore.module.css";
 import PropTypes from "prop-types";
 
 function LoadBtnMore({ onClick, children }) {
