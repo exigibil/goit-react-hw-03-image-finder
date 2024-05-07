0aa4bc4243a8721440124fae8b254e5263359b90
exigibil <adrian.topa04@gmail.com>
2024-05-07 21:37:35 +0300

    update json

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -5,9 +5,11 @@ import PropTypes from "prop-types";
 function Modal({ imageUrl, onClose }) {
   const closeModal = () => {
     onClose();
+    document.querySelector(".imageGalleryContainer").style.overflow = "auto";
   };
 
   const openModal = () => {
+    document.querySelector(".imageGalleryContainer").style.overflow = "hidden"
     const instance = basicLightbox.create(`
       <img src="${imageUrl}" width="800" height="600">
     `);
@@ -16,7 +18,7 @@ function Modal({ imageUrl, onClose }) {
 
   return (
     <div className={styles.overlay} onClick={closeModal}>
-      <div className={styles.imgContainer} onClick={closeModal}>
+      <div className={styles.imgContainer} onClick={(e) => e.stopPropagation()}>
         <img src={imageUrl} onClick={openModal} alt="Click to open" />
       </div>
     </div>
