57ad4315b9f21270c8f8f577f307eb42a09f0cfb
exigibil <adrian.topa04@gmail.com>
2024-05-08 19:47:12 +0300

    update vite js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base =
+  process.env.NODE_ENV === 'production'
+    ? '/goit-react-hw-03-image-finder/'
+    : '/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [react()],
 })
